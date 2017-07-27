b74023af8b4400e8040f0c01bc100355aa91ea9b
zhangke <zhangk005@foresealife.com>
2017-07-27 14:47:43 +0800

    zk 增加props onIndexChanged

--- src/index.js
+++ src/index.js
@@ -122,7 +122,11 @@ export default class extends Component {
     dotStyle: PropTypes.object,
     activeDotStyle: PropTypes.object,
     dotColor: PropTypes.string,
-    activeDotColor: PropTypes.string
+    activeDotColor: PropTypes.string,
+       /**
+          * Called when the index has changed because the user swiped.
+          */
+      onIndexChanged: PropTypes.func
   }
 
   /**
@@ -147,7 +151,8 @@ export default class extends Component {
     autoplay: false,
     autoplayTimeout: 2.5,
     autoplayDirection: true,
-    index: 0
+    index: 0,
+    onIndexChanged: () => null
   }
 
   /**
@@ -178,6 +183,11 @@ export default class extends Component {
     this.autoplayTimer && clearTimeout(this.autoplayTimer)
     this.loopJumpTimer && clearTimeout(this.loopJumpTimer)
   }
+
+  componentWillUpdate (nextProps, nextState) {
+       // If the index has changed, we notify the parent via the onIndexChanged callback
+          if (this.state.index !== nextState.index) this.props.onIndexChanged(nextState.index)
+      }
 
   initState (props, setOffsetInState) {
     // set the current state
